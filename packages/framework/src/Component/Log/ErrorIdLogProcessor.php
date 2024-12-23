6ed966be174bc38c77f5921cf61e2eb2370967ed
Jan Górecki <jan.gorecki@shopsys.com>
2024-12-23 05:23:36 +0100

    refactor getting errorId for exceptions

---
+++ packages/framework/src/Component/Log/ErrorIdLogProcessor.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Component\Log;
+
+use Monolog\LogRecord;
+use Shopsys\FrameworkBundle\Component\Error\ErrorIdProvider;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Contracts\Service\ResetInterface;
+
+final class ErrorIdLogProcessor implements EventSubscriberInterface, ResetInterface
+{
+    private ?string $errorId = null;
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Component\Error\ErrorIdProvider $errorIdProvider
+     */
+    public function __construct(
+        public readonly ErrorIdProvider $errorIdProvider,
+    ) {
+    }
+
+    /**
+     * @param \Monolog\LogRecord $record
+     * @return \Monolog\LogRecord
+     */
+    public function __invoke(LogRecord $record): LogRecord
+    {
+        if ($this->errorId !== null) {
+            $record['extra']['errorId'] = $this->errorId;
+        }
+
+        return $record;
+    }
+
+    public function onException(): void
+    {
+        $this->errorId = $this->errorIdProvider->getErrorId();
+    }
+
+    public function reset(): void
+    {
+        $this->errorId = null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => ['onException', 1],
+        ];
+    }
+}

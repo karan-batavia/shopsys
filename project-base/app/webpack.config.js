c6645065f2ba455422a82fa7e45335f1a5967ff6
Jan Górecki <jan.gorecki@shopsys.com>
2025-01-13 21:04:54 +0100

    Upgrade webpack encore bundle

--- project-base/app/webpack.config.js
+++ project-base/app/webpack.config.js
@@ -15,10 +15,6 @@ Encore
     .setManifestKeyPrefix('web')
     .cleanupOutputBeforeBuild()
     .autoProvidejQuery()
-    // hp entry?
-    // order entry?
-    // product entry?
-    // cart entry?
     .addEntry('admin', './assets/js/admin/admin.js')
     .splitEntryChunks()
     .enableSingleRuntimeChunk()
@@ -75,9 +71,7 @@ Encore
     .enablePostCssLoader()
 ;
 
-const config = Encore.getWebpackConfig();
-
-config.resolve.alias = {
+Encore.addAliases({
     'jquery-ui': 'jquery-ui/ui/widgets',
     'framework': '@shopsys/framework/js',
     'jquery': path.resolve(path.join(__dirname, 'node_modules', 'jquery')),
@@ -85,5 +79,6 @@ config.resolve.alias = {
     'bazinga-translator': path.resolve(path.join(__dirname, 'node_modules', 'bazinga-translator')),
     'jquery-ui-nested-sortable': path.resolve(path.join(__dirname, 'node_modules', 'nestedSortable')),
     'icons': path.resolve(path.join(__dirname, 'assets/icons'))
-};
-module.exports = config;
+});
+
+module.exports = Encore.getWebpackConfig();

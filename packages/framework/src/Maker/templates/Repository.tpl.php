441be3340fe55f71bf29f712553c5d7d176d8c43
Rostislav Vitek <rostislav.vitek@shopsys.com>
2025-02-19 17:04:32 +0100

    create basic entity maker
    - fields configuration will be added in the later commits

--- packages/framework/src/Maker/templates/Repository.tpl.php
+++ packages/framework/src/Maker/templates/Repository.tpl.php
@@ -1,4 +1,5 @@
 <?= "<?php\n"; ?>
+<?php /** @var \Shopsys\FrameworkBundle\Maker\EntityConfig\EntityConfig $entity_config */ $entity_config ?>
 
 declare(strict_types=1);
 
@@ -16,24 +17,24 @@ class <?= $class_name; ?>
     ) {
     }
 
-    protected function get<?= $entity_name; ?>Repository(): EntityRepository
+    protected function get<?= $entity_config->entityName; ?>Repository(): EntityRepository
     {
-        return $this->em->getRepository(<?= $entity_name; ?>::class);
+        return $this->em->getRepository(<?= $entity_config->entityName; ?>::class);
     }
 
-    public function getById(int $<?= lcfirst($entity_name); ?>Id): <?= $entity_name; ?>
+    public function getById(int $<?= lcfirst($entity_config->entityName); ?>Id): <?= $entity_config->entityName; ?>
     {
-        $<?= lcfirst($entity_name); ?> = $this->findById($<?= lcfirst($entity_name); ?>Id);
+        $<?= lcfirst($entity_config->entityName); ?> = $this->findById($<?= lcfirst($entity_config->entityName); ?>Id);
 
-        if ($<?= lcfirst($entity_name); ?> === null) {
-            throw new <?= $entity_name; ?>NotFoundException('<?= $entity_name; ?> with ID ' . $<?= lcfirst($entity_name); ?>Id . ' not found.');
+        if ($<?= lcfirst($entity_config->entityName); ?> === null) {
+            throw new <?= $entity_config->entityName; ?>NotFoundException('<?= $entity_config->entityName; ?> with ID ' . $<?= lcfirst($entity_config->entityName); ?>Id . ' not found.');
         }
 
-        return $<?= lcfirst($entity_name); ?>;
+        return $<?= lcfirst($entity_config->entityName); ?>;
     }
 
-    public function findById(int $<?= lcfirst($entity_name); ?>Id): ?<?= $entity_name; ?>
+    public function findById(int $<?= lcfirst($entity_config->entityName); ?>Id): ?<?= $entity_config->entityName; ?>
     {
-        return $this->get<?= $entity_name; ?>Repository()->find($<?= lcfirst($entity_name); ?>Id);
+        return $this->get<?= $entity_config->entityName; ?>Repository()->find($<?= lcfirst($entity_config->entityName); ?>Id);
     }
 }

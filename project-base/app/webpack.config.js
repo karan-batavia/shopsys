d8b47e7c6f1a61abfad99d71b240b7bcd506a256
Martin Grossmann <martin@vyvoj.net>
2025-02-20 15:18:29 +0100

    WIP

--- project-base/app/webpack.config.js
+++ project-base/app/webpack.config.js
@@ -9,8 +9,7 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
-Encore
-    .setOutputPath('web/build/')
+Encore.setOutputPath('web/build/')
     .setPublicPath((process.env.CDN_DOMAIN ? process.env.CDN_DOMAIN : '') + '/build')
     .setManifestKeyPrefix('web')
     .cleanupOutputBeforeBuild()
@@ -25,60 +24,59 @@ Encore
     })
     .addRule({
         test: /\.svg/,
-        type: 'asset/source'
+        type: 'asset/source',
     })
     .enableBuildNotifications()
     .configureWatchOptions(function (watchOptions) {
         watchOptions.ignored = '**/*.json';
     })
-    .addPlugin(new CopyPlugin({
-        patterns: [
-            {
-                from: 'web/bundles/fpjsformvalidator',
-                to: '../../assets/js/bundles/fpjsformvalidator',
-                force: true
-            },
-            {
-                from: sources.getFrameworkNodeModulesDir() + '/public/admin',
-                to: '../../web/public/admin',
-                force: true
-            },
-            {
-                from: 'assets/public',
-                to: '../../web/public',
-                force: true
-            },
-            {
-                from: 'assets/extra',
-                to: '../../web',
-                force: true
-            }
-        ]
-    }))
-    .addPlugin(new LiveReloadPlugin())
-;
+    .addPlugin(
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'web/bundles/fpjsformvalidator',
+                    to: '../../assets/js/bundles/fpjsformvalidator',
+                    force: true,
+                },
+                {
+                    from: sources.getFrameworkNodeModulesDir() + '/public/admin',
+                    to: '../../web/public/admin',
+                    force: true,
+                },
+                {
+                    from: 'assets/public',
+                    to: '../../web/public',
+                    force: true,
+                },
+                {
+                    from: 'assets/extra',
+                    to: '../../web',
+                    force: true,
+                },
+            ],
+        }),
+    )
+    .addPlugin(new LiveReloadPlugin());
 
-Encore
-    .addEntry('admin-style', './assets/styles/admin/main.less')
+Encore.addEntry('admin-style', './assets/styles/admin/main.less')
     .addEntry('admin-wysiwyg', './assets/styles/admin/wysiwyg.less')
     .addPlugin(
         new StylelintPlugin({
             configFile: '.stylelintrc',
-            files: 'assets/styles/**/*.less'
-        })
+            files: 'assets/styles/**/*.less',
+        }),
     )
     .enableLessLoader()
-    .enablePostCssLoader()
-;
+    .enablePostCssLoader();
 
 Encore.addAliases({
     'jquery-ui': 'jquery-ui/ui/widgets',
-    'framework': '@shopsys/framework/js',
-    'jquery': path.resolve(path.join(__dirname, 'node_modules', 'jquery')),
-    'jquery-ui-styles': path.resolve(path.join(__dirname, 'node_modules', 'jquery-ui')),
-    'bazinga-translator': path.resolve(path.join(__dirname, 'node_modules', 'bazinga-translator')),
-    'jquery-ui-nested-sortable': path.resolve(path.join(__dirname, 'node_modules', 'nestedSortable')),
-    'icons': path.resolve(path.join(__dirname, 'assets/icons'))
+    framework: '@shopsys/framework/js',
+    jquery: path.resolve(path.join(sources.getNodeModulesDir(), 'jquery')),
+    'jquery-ui-styles': path.resolve(path.join(sources.getNodeModulesDir(), 'jquery-ui')),
+    'bazinga-translator': path.resolve(path.join(sources.getNodeModulesDir(), 'bazinga-translator')),
+    'jquery-ui-nested-sortable': path.resolve(path.join(sources.getNodeModulesDir(), 'nestedSortable')),
+    icons: path.resolve(path.join(__dirname, 'assets/icons')),
 });
 
 module.exports = Encore.getWebpackConfig();
